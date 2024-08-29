2fc261c3292737c451845715d9f315f9840b683d
Mahammad1122 <mahammadali2307@gmail.com>
2024-08-29 18:30:35 +0530

    done logout of student     done header file     done urlhiding

--- index.php
+++ index.php
@@ -15,41 +15,7 @@
     </script>
 </head>
 <body>
-<header>
-        <span> <a href="index.php">Exam25</a> </span>
-        <nav>
-            <a href="index.php">HOME</a>
-            <a href="student_login.php">EXAM</a>
-            <a href="#">EVENTS</a>
-            <a href="exam25_about.php">ABOUT</a>
-            <a href="#">CONTACT</a>
-               <!-- user profile menu -->
-            <?php 
-                if(!isset($_SESSION['userId']) && empty($_SESSION['userId']))
-                {
-            ?>
-                <a href="student_reg.php">REGISTER</a>
-            <?php
-                }
-                else
-                {
-            ?>
-            <img src="image/userprofile/user.png" class="img" alt="">
-            <div class="user-menu">
-                <span class="profile"> <img src="image/userprofile/profile.png"> My profile</span>
-                <hr width="100%">
-                <span class="user-data">
-                    <span class="user-name"><?php echo $_SESSION['userName'][1]; ?></span>
-                    <span class="user-email"><?php echo $_SESSION['userMail']; ?></span>
-                </span>
-                <hr width="100%">
-                <span class="logout"> <img src="image/userprofile/logout.png"> Log-out</span>
-            </div>
-            <?php
-                }
-            ?>
-        </nav>
-    </header>
+    <?php include_once 'include/header.php'; ?>
     <section>
         <div class="content">
             <span>Welcome to Exam25,</span>
@@ -64,7 +30,7 @@
 
     </footer>
     <script>
-        <?php include_once 'js/exam25.js'; ?>
+        <?php // include_once 'js/exam25.js'; ?>
     </script>
 </body>
 </html>
