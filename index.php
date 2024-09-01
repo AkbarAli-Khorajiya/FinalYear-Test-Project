a11d85bc3eddd782f4a76ddbb55ee1325e6fd06a
Mahammad1122 <mahammadali2307@gmail.com>
2024-09-01 13:32:12 +0530

    add Roboto fonts ofline

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exam25</title>
     <style>
+        <?php include 'css/font/font.css';?>
         <?php include 'css/exam25.css' ?>
     </style>
     <script>
