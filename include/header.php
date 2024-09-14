077177faba03015c279192692747f00190743f22
Mahammad1122 <mahammadali2307@gmail.com>
2024-09-14 22:20:21 +0530

    new landing page design implemented

--- include/header.php
+++ include/header.php
@@ -1,217 +1,248 @@
 <style>
-header
-{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 10vh;
-    width: 100vw;
-    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.178);
-}
-header span
-{
-    margin-left: 4vh;
-    font-family:'Roboto-Bold', sans-serif;
-    font-size: 3.5vw;
-    /* font-weight: 600; */
-}
-header span a
-{
-    color: rgb(38, 50, 56);
-    text-decoration: none;
-}
-header nav
-{
-    font-size: 1.6vw;
-    align-self: center;
-    margin-right: 4vh;
-    font-family: 'Roboto-Regular', sans-serif;
-}
-header nav a
-{
-    color: #455a64;
-    margin: 0.8vh;
-    text-decoration: none;
-}
-nav  img
-{
-    /* height: 1.7vw; */
-    width: 30px;
-    position: relative;
-    top: 7px;
-    transition: 200ms linear 0s all;
-    border-radius: 100%;
-}
-nav img:hover
-{
-    box-shadow: 0px 0px 4px rgba(85, 85, 85, 0.658);
-}
-nav .user-menu
-{
-    z-index: 1;
-    display: none;
-    flex-direction: column;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    font-size: 1.2vw;
-    list-style: none;
-    position: fixed;
-    top: 12%;
-    right: 2.5%;
-    padding: 10px 20px;
-    color: white;
-    background-color: rgb(73, 72, 72);
-    border-radius: 20px 0px 20px 20px;
-    animation: up 0.5s;
-}
-@keyframes up {
-    0%{
-        opacity: 0;visibility: hidden;
+    header
+    {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0px 20px;
     }
-    100%{
-        opacity: 1;visibility: visible;
+    header .logo
+    {
+        font-size: 46px;
+        font-family: 'Ubuntu-Bold' , sans-serif;
     }
-}
-.user-menu span
-{
-    margin: 0;
-    font-size: 1.2vw;
-    padding: 5px 10px;
-    display: flex;
-    gap: 15px;
-    /* flex-direction: row; */
-    align-items: center;
-    width: 100%;
-    justify-content: flex-start;
-    /* align-items:center; */
-    /* border-bottom: 1px solid white; */
-}
-.user-menu img
-{
-    all: unset;
-    width: 30px;
-    margin: 0px;
-    padding: 0px;
-    /* align-self: center; */
-    /* text-align: center; */
-}
-.user-menu .user-data
-{
-    margin: 10px 0px;
-    padding: 10px 2px;
-    display: flex;
-    justify-content: center;
-    flex-flow: column wrap;
-    align-items: flex-start;
-    background-color: rgb(58, 58, 58);
-    border-radius: 10px;
-    /* box-shadow: 0px 0px 8px rgba(158, 158, 158, 0.466); */
-}
-.user-data span
-{
-    all: unset;
-}
-.user-data .user-name
-{
-    padding: 0px 5px;
-}
-.user-data .user-email
-{
-    font-size: 13px;
-    align-self: flex-end;
-    padding: 0px 10px;
-}
-.user-menu .logout
-{
-    /* text-align: right; */
-    padding: 5px 0px;
-    justify-content: flex-end;
-    cursor: pointer;
-    transition: 200ms linear 0s all;
-    &:hover{
-        opacity: 70%;
+    .logo span
+    {
+        color: var(--primary-color);
     }
-}
-nav .user-menu:after {
-    content: '';
-    display: block;
-    width: 0px;
-    left: 95.5%;
-    border-top: 8px solid #1C242B;
-    border-left: 8px solid transparent;
-    border-right: 8px solid transparent;
-    position: absolute;
-    top: -4.5%;
-    margin-left: -5px;
-    transform: rotate(180deg);
-  }
+    header nav
+    {
+        height: fit-content;
+        width: fit-content;
+        display: flex;
+        justify-content: center;
+        /* align-items: center; */
+        flex-direction: row;
+        font-family: 'Roboto' , sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+    nav a
+    {
+        color: var(--nav-color);
+        padding: 10px;
+        font-size: 28px;
+        text-decoration: none;
+        transition: 200ms linear 0s all;
+        position: relative;
+        &:hover{
+            color: var(--secondary-color);
+        }
+    }
+    nav .login-btn
+    {
+        align-self: center;
+        font-size: 22px;
+        color: #ffff;
+        padding: 4px 20px;
+        background-color: var(--primary-color);
+        border-radius: 50px;
+        transition: 200ms ease all 0s;
+        &:hover
+        {
+            background-color:#7376b6;
+            text-decoration: none;
+            color: #ffff;
+        }
+    }
+    nav .user-btn
+    {
+        /* display: inline-block; */
+        padding:10px 10px 0px 10px;
+        svg
+        {
+            height: 30px;
+            width: 30px;
+            /* position: relative; */
+            /* top:8px; */
+            background-color: var(--primary-color);
+            fill: #ffffff;
+            border-radius: 50px;
+            padding: 3px;
+            transition: 200ms ease all 0s;
+            &:hover
+            {
+                background-color: var(--secondary-color);
+                fill: var(--primary-color);
+            }
+        }
+    }
+    nav .profile-menu
+    {
+        position: fixed;
+        right: 30px;
+        top: 60px;
+        background-color: #ffffff;
+        width: fit-content;
+        font-family: 'Roboto-Regular' , sans-serif;
+        font-style: normal;
+        border-radius: 15px;
+        border: 0.5px solid #1a1a2738;
+        z-index: 99;
+        padding-bottom: 5px;
+        animation: 200ms fade linear;
+        .user-profile
+        {
+            /* width:fit-content; */
+            border-bottom: 0.5px solid #1a1a2738;
+            display: flex;
+            flex-flow: row;
+            padding: 10px;
+            margin-bottom: 5px;
+        }
+        .user-img img
+        {
+            width: 45px;
+            height: 45px;
+            padding: 0px 10px 0px 0px;
+        }
+        .user-info
+        {
+            margin-top: 2px;
+        }
+        .user-info span
+        {
+            display: block;
+        }
+        .user-info .name
+        {
+            font-size: 18px;
+            font-weight: 600;
+        }
+        .user-info .email
+        {
+            font-weight: 400;
+            font-size: 14px;
+            color:#1a1a27ab;
+        }
+        .menu-item
+        {
+            padding: 5px 0px 5px 15px;
+            width: fit-content;
+            display: flex;
+            flex-flow: row;
+            align-items: center;
+        }
+        .menu-item .icon
+        {
+            padding: 0px 15px 0px 0px;
+        }
+        .menu-item .icon img
+        {
+            height: 18px;
+            width: 18px;
+        }
+        .menu-item .title
+        {
+            font-size: 18px;
+            font-weight: 400;
+        }
+    }
+    @keyframes fade {
+        from{
+            opacity: 0;
+            visibility: hidden;
+        }
+        to{
+            opacity: 1;
+            visibility: visible;
+        }
+    }
+    /* Nav bar Animation link */
+    nav a::before{
+        content: "";
+        width: 81%;
+        height: 3px;
+        position: absolute;
+        left: 10px;
+        bottom: 10px;
+        background: #fff;
+        transition: 0.5s transform ease;
+        transform: scale3d(0,1,1);
+        transform-origin: 0 50%;
+    }
+    nav a:hover::before{
+        transform: scale3d(1,1,1);
+    }
+    nav a::before{
+        background: var(--primary-color);
+        transform-origin: 100% 50%;
+    }
+    nav a:hover::before{
+        transform-origin: 0 50%;
+    }
+
+    nav .login-btn::before
+    {
+        content: none;
+    }
+    nav .user-btn::before
+    {
+        content: none;
+    }
+    nav a:nth-child(5)::before
+    {
+        width: 86%;
+    }
+    nav a:nth-child(2)::before
+    {
+        width: 78%;
+    }
 </style>
 <header>
-    <span> <a href="index">Exam25</a> </span>
+    <div class="logo"><span>Exam</span>Zone</div>
     <nav>
-        <a href="index.php">HOME</a>
-        <a href="student_login.php">EXAM</a>
-        <a href="#">EVENTS</a>
-        <a href="exam25_about">ABOUT</a>
-        <a href="#">CONTACT</a>
-            <!-- user profile menu -->
+        <a href="#">Home</a>
+        <a href="#">Exam</a>
+        <a href="#">Events</a>
+        <a href="#">About</a>
         <?php 
             if(!isset($_SESSION['stdLogin']) && empty($_SESSION['stdLogin']))
             {
         ?>
-            <a href="student_reg">REGISTER</a>
+            <a href="student_reg.php" class="login-btn">Login</a>
         <?php
             }
             else
             {
         ?>
-        <img src="image/userprofile/user.png" class="img" alt="">
-        <div class="user-menu">
-            <span class="profile"> <img src="image/userprofile/profile.png"> My profile</span>
-            <hr width="100%">
-            <span class="user-data">
-                <span class="user-name"><?php echo $_SESSION['userName'][1]; ?></span>
-                <span class="user-email"><?php echo $_SESSION['userMail']; ?></span>
-            </span>
-            <hr width="100%">
-            <span class="logout"> <img src="image/userprofile/logout.png"> Log-out</span>
-        </div>
+            <a href="#" class="user-btn">
+                <svg  viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.25 9C8.25 6.92893 9.92893 5.25 12 5.25C14.0711 5.25 15.75 6.92893 15.75 9C15.75 11.0711 14.0711 12.75 12 12.75C9.92893 12.75 8.25 11.0711 8.25 9ZM12 6.75C10.7574 6.75 9.75 7.75736 9.75 9C9.75 10.2426 10.7574 11.25 12 11.25C13.2426 11.25 14.25 10.2426 14.25 9C14.25 7.75736 13.2426 6.75 12 6.75Z" />
+                    <path  fill-rule="evenodd" clip-rule="evenodd" d="M1.25 12C1.25 6.06294 6.06294 1.25 12 1.25C17.9371 1.25 22.75 6.06294 22.75 12C22.75 17.9371 17.9371 22.75 12 22.75C6.06294 22.75 1.25 17.9371 1.25 12ZM12 2.75C6.89137 2.75 2.75 6.89137 2.75 12C2.75 14.5456 3.77827 16.851 5.4421 18.5235C5.6225 17.5504 5.97694 16.6329 6.68837 15.8951C7.75252 14.7915 9.45416 14.25 12 14.25C14.5457 14.25 16.2474 14.7915 17.3115 15.8951C18.023 16.6329 18.3774 17.5505 18.5578 18.5236C20.2217 16.8511 21.25 14.5456 21.25 12C21.25 6.89137 17.1086 2.75 12 2.75ZM17.1937 19.6554C17.0918 18.4435 16.8286 17.5553 16.2318 16.9363C15.5823 16.2628 14.3789 15.75 12 15.75C9.62099 15.75 8.41761 16.2628 7.76815 16.9363C7.17127 17.5553 6.90811 18.4434 6.80622 19.6553C8.28684 20.6618 10.0747 21.25 12 21.25C13.9252 21.25 15.7131 20.6618 17.1937 19.6554Z" />
+                </svg>
+            </a>
+            <div class="profile-menu">
+                <div class="user-profile">
+                    <div class="user-img">
+                        <img src="image/userprofile/male1.png" alt="">
+                    </div>
+                    <div class="user-info">
+                        <span class="name"> Mahammad ali </span>
+                        <span class="email"> mahammadali2307@gmail.com </span>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <span class="icon"><img  src="image/userprofile/progress.svg" alt=""></span>
+                    <span class="title">My Progress</span>
+                </div>
+                <div class="menu-item logout">
+                    <span class="icon"><img src="image/userprofile/logout.svg" alt=""></span>
+                    <span class="title">Logout</span>
+                </div>
+            </div>
         <?php
             }
         ?>
     </nav>
 </header>
-<script>
-    //------show profile menu-------//
-$("nav .img").click(()=>{
-    $("nav .user-menu").css('display', 'flex');
-});
-// ------hide profile menu-------//
-let img = document.querySelector(".img");
-let menu = document.querySelector(".user-menu");
-img.addEventListener("click",()=>{
-    menu.style.display = 'flex';
-});
-document.addEventListener("click",()=>{
-    let imgClicked = img.contains(event.target);
-    let menuClicked = menu.contains(event.target);
-    if(!imgClicked && !menuClicked)
-    {
-        menu.style.display = "none";
-    }
-});
-// ----------Log Out----------//
-$(".logout").on("click", function () {
-    $.ajax({
-        type: "POST",
-        url: "include/FrontEndOperation.php?ch=3",
-        data: {logout : 1},
-        success: function (response) {
-           if(response == 1)
-           {
-                location.href ='index.php';
-           }
-        }
-      });
-});
-</script>
--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exam25</title>
-    <style>
-        <?php include 'css/font/font.css';?>
-        <?php include 'css/exam25.css' ?>
+    <style type="text/css">
+        <?php 
+            include 'css/exam25.css'; 
+        ?>
     </style>
     <script>
         <?php include_once 'js/jquery-3.7.1.min.js';?>
@@ -18,20 +19,25 @@
 <body>
     <?php include_once 'include/header.php'; ?>
     <section>
-        <div class="content">
-            <span>Welcome to Exam25,</span>
-            <p>the online platform for taking exams in various subjects. Whether you are a student, a professional, or a lifelong learner, Exam25 can help you test your knowledge, track your progress, and challenge yourself with harder questions. Exam25 is easy to use, reliable, and fun. Join us today and start your online exam journey!</p>
-            <a href="student_reg.php">Let's Start</a>
-        </div>
         <div class="container">
-            <img src="image/homepage.png" >
+            <div class="content">
+                <span>Rise above</span>
+                <span>the competition</span>
+                <span>in the ExamZone</span>
+            </div>
+            <span class="btn">
+                <a href="#">Get Started</a>
+            </span>
+        </div>
+        <div class="img">
+            <img src="image/Exams-bro.png" alt="">
         </div>
     </section>
     <footer>
 
     </footer>
     <script>
-        <?php // include_once 'js/exam25.js'; ?>
+        <?php include_once 'js/exam25.js'; ?>
     </script>
 </body>
 </html>
