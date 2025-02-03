2eeb24164d1d6ea597154e5b28776f99fe477d1f
AkbarAli <akbarali.m.khorajiya@gmail.com>
2025-02-03 12:52:31 +0530

    new module

--- student_login.php
+++ student_login.php
@@ -1,36 +1,36 @@
 <?php
-    include 'include/database.php';
-    $alert='';
-    if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['login']))
-    {
-        $username = $_POST['uname'];
-        $password = $_POST['password'];
-        $query = "SELECT * FROM student_reg WHERE u_name = '$username' AND password = '$password'";
-        $result = mysqli_query($link, $query);
-        if (mysqli_num_rows($result) > 0) 
-        {
-            session_start();
-            $_SESSION['student_login']= true;
-            $row = mysqli_fetch_assoc($result);
-            $_SESSION['s_id']=$row['reg_id'];
-            header('location:exam_main.php');
-        } 
-        else 
-        {
-            $alert="Login failed";
-        }
+include 'include/database.php';
+$alert = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+    $username = $_POST['uname'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM student_reg WHERE u_name = '$username' AND password = '$password'";
+    $result = mysqli_query($link, $query);
+    if (mysqli_num_rows($result) > 0) {
+        session_start();
+        $_SESSION['student_login'] = true;
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['s_id'] = $row['reg_id'];
+        header('location:exam_main.php');
+    } else {
+        // sAJSHa
+        $alert = "Login failed";
     }
-    mysqli_close($link);
+}
+mysqli_close($link);
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exam25</title>
     <style>
-        <?php include 'css/student_login.css'; ?>
+    <?php include 'css/student_login.css';
+    ?>
     </style>
 </head>
+
 <body>
     <div class="box">
         <img src="image/8.png">
@@ -44,12 +44,13 @@
                 <li> <input type="password" name="password" placeholder="Enter Password" required> </li>
                 <li> <input type="submit" value="LOGIN" name="login"></li>
                 <li> <a href="#">Forget Password?</a> </li>
-                <li class="error"><?php echo $alert;?></li>
+                <li class="error"><?php echo $alert; ?></li>
             </ul>
         </form>
     </div>
     <script>
-        <?php include 'js/exam25_exam1.js' ?>
+    <?php include 'js/exam25_exam1.js' ?>
     </script>
 </body>
+
 </html>
