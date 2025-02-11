a6e79a09e8074c0cbaffabdef992d9746f92241f
Mahammad1122 <mahammadali2307@gmail.com>
2025-02-11 10:43:23 +0530

    extrafile deleted

--- admin/include/que_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-$database = 'exam_test';
-
-    $link = mysqli_connect($server,$username,$password,$database) or die('Not connected');
-?>
