6264f7b8c17e9ecfe78fbb5a9ea63cb6e04ae3df
Mahammad1122 <mahammadali2307@gmail.com>
2025-03-20 11:14:42 +0530

    latest

--- user_submit.php
+++ user_submit.php
@@ -22,7 +22,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Test Result</title>
+  <title><?php if(str_contains(strtoupper($testName),"TEST")){ echo strtoupper($testName) ."RESULT";} else {echo strtoupper($testName) ." TEST RESULT";}?></title>
   <script src="js/jquery-3.7.1.min.js"></script>
   <style>
     <?php include_once 'css/user_submit.css'; ?>
